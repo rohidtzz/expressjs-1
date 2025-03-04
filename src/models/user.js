68ede55dcc16cf81293af881e5aa35ec96162b27
rohidtzz <gemersrasta@gmail.com>
2025-03-05 05:49:45 +0700

    prettier

--- src/models/user.js
+++ src/models/user.js
@@ -3,37 +3,37 @@ import db from "../config/db.js";
 
 
 const User = db.define(
-    "users",
-    {
-        id: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-        },
-        name: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        email: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-        },
-        username: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-        },
-        password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-      },
-      {
-        timestamps: true,
-        underscored : true,
-        tableName : "users"
-      }
+  "users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+    underscored: true,
+    tableName: "users"
+  }
 );
 
 export default User
